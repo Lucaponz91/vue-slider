74a2506bc8aa9c21f514f41331bf597a2a9e02e3
Lucaponz91 <lucapo1991@msn.com>
2022-08-24 16:16:33 +0200

    add nextSlide

--- js/slider.js
+++ js/slider.js
@@ -7,3 +7,26 @@ const slides = [
 	'./img/04.jpg',
 	'./img/05.jpg',
 ]
+
+const app = new Vue({
+    el: '#root',
+	data: {
+		slides,
+		index: 0,
+
+	},
+	methods:{
+		prevSlide() {
+			
+		},
+		nextSlide(){
+			if (this.index === this.slides.length - 1) {
+				this.index = 0} else {
+					this.index++
+				}
+
+			
+		}
+	}
+    
+})
